3617f779c75033b19cd91eae1cb3a56c9ea0ab4d
GittingBoor <benedikt.kolodziej@web.de>
2026-06-24 17:46:56 +0200

    Nearest Neighbours through Embedding

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -62,12 +62,26 @@ def sample_anomaly_results():
     }
 
 
+@pytest.fixture
+def sample_nn_results():
+    return {
+        "1": {"2": 0.083},
+        "2": {"1": 0.043},
+    }
+
+
 def test_create_DataOverview(
-    sample_metadata_results, sample_umap_results, sample_anomaly_results
+    sample_metadata_results,
+    sample_umap_results,
+    sample_anomaly_results,
+    sample_nn_results,
 ):
     # TODO: anomaly ergänzen
     response = pipe.create_DataOverview(
-        sample_metadata_results, sample_umap_results, sample_anomaly_results
+        sample_metadata_results,
+        sample_umap_results,
+        sample_anomaly_results,
+        sample_nn_results,
     )
 
     assert len(response) == 2
@@ -75,15 +89,28 @@ def test_create_DataOverview(
     assert response[0].label == "laughing"
     assert response[0].umap_x == 0.1
     assert response[0].anomalie_LOF == 1.2
+    assert response[0].nearest_neighbors == {"2": 0.083}
+
+
+def test_create_data_overview_skips_missing_nn(
+    sample_metadata_results, sample_umap_results, sample_anomaly_results
+):
+    nn_results = {}
+
+    result = pipe.create_DataOverview(
+        sample_metadata_results, sample_umap_results, sample_anomaly_results, nn_results
+    )
+
+    assert result == []
 
 
 def test_create_data_overview_skips_missing_metadata(
-    sample_umap_results, sample_anomaly_results
+    sample_umap_results, sample_anomaly_results, sample_nn_results
 ):
     metadata_results = {}
 
     result = pipe.create_DataOverview(
-        metadata_results, sample_umap_results, sample_anomaly_results
+        metadata_results, sample_umap_results, sample_anomaly_results, sample_nn_results
     )
 
     assert result == []
@@ -103,6 +130,7 @@ def test_save_results_as_json(tmp_path):
             anomalie_LOF=0.0,
             anomalie_isolation_forest_label="unknown",
             anomalie_LOF_label="unknown",
+            nearest_neighbors={"uuid_2": 0.083},
         )
     ]
 
@@ -115,4 +143,5 @@ def test_save_results_as_json(tmp_path):
     assert "uuid_1" in saved
     assert saved["uuid_1"]["umap_x"] == 1.0
     assert saved["uuid_1"]["label"] == "laughing"
+    assert saved["uuid_1"]["nearest_neighbors"] == {"uuid_2": 0.083}
     assert "uuid" not in saved["uuid_1"]
